2d12652bbbc1833ce15ddd0318997beb6e3d8333
Marek Kaput <marek.kaput@outlook.com>
2017-04-26 19:52:49 +0200

    add @throws attributes

--- core/src/main/scala-2.12/edu/scalanus/ScalanusScriptEngine.scala
+++ core/src/main/scala-2.12/edu/scalanus/ScalanusScriptEngine.scala
@@ -4,18 +4,30 @@ import java.io.Reader
 import javax.script._
 
 class ScalanusScriptEngine private[scalanus](val factory: ScalanusScriptEngineFactory)
-  extends AbstractScriptEngine with Invocable {
+  extends AbstractScriptEngine with Compilable with Invocable {
 
+  @throws[ScriptException]
+  override def compile(script: String): CompiledScript = ???
+
+  @throws[ScriptException]
+  override def compile(script: Reader): CompiledScript = ???
+
+  @throws[ScriptException]
   override def eval(script: String, context: ScriptContext): AnyRef = ???
 
+  @throws[ScriptException]
   override def eval(reader: Reader, context: ScriptContext): AnyRef = ???
 
   override def createBindings(): Bindings = new SimpleBindings()
 
   override def getFactory: ScriptEngineFactory = factory
 
-  override def invokeMethod(thiz: scala.Any, name: String, args: AnyRef*): AnyRef = ???
+  @throws[ScriptException]
+  @throws[NoSuchMethodException]
+  override def invokeMethod(receiver: scala.Any, name: String, args: AnyRef*): AnyRef = ???
 
+  @throws[ScriptException]
+  @throws[NoSuchMethodException]
   override def invokeFunction(name: String, args: AnyRef*): AnyRef = ???
 
   override def getInterface[T](returnType: Class[T]): T = ???
