89fadb0140dadd01199cf57c1806b39b1d6f9e1c
Marek Kaput <marek.kaput@outlook.com>
2017-05-23 19:57:56 +0200

    minor style

--- src/main/scala/edu/scalanus/parser/ThrowingErrorListener.scala
+++ src/main/scala/edu/scalanus/parser/ThrowingErrorListener.scala
@@ -5,11 +5,13 @@ import org.antlr.v4.runtime.{BaseErrorListener, RecognitionException, Recognizer
 
 object ThrowingErrorListener extends BaseErrorListener {
   @throws[ParseCancellationException]
-  override def syntaxError(recognizer: Recognizer[_, _],
-                           offendingSymbol: scala.Any,
-                           line: Int,
-                           charPositionInLine: Int,
-                           msg: String,
-                           e: RecognitionException): Unit =
+  override def syntaxError(
+    recognizer: Recognizer[_, _],
+    offendingSymbol: scala.Any,
+    line: Int,
+    charPositionInLine: Int,
+    msg: String,
+    e: RecognitionException
+  ): Unit =
     throw new ParseCancellationException(s"line $line:$charPositionInLine $msg")
 }
